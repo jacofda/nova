83b88c9c9964bfcd965c522c5792fba413019190
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2023-04-28 21:11:07 +0200

    half-footer

--- src/routes/chi-siamo/+page.svelte
+++ src/routes/chi-siamo/+page.svelte
@@ -1,7 +1,54 @@
 <script>
+	import { Col, Row } from 'sveltestrap';
+	import SectionTitle from '$lib/components/elements/SectionTitle.svelte';
 </script>
 
 <svelte:head>
-	<title>Chi Siamo</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Chi Siamo | Novainfissi Ospedaletto Trento</title>
+	<meta
+		name="description"
+		content="Nova Infissi standard di qualità e affibilità Ditta creata da Cenci Giuseppe Mirko nel 1999 ..."
+	/>
+	<link rel="canonical" href="https://www.novainfissi.it/novainfissi-cenci-ospedaletto" />
 </svelte:head>
+
+<SectionTitle title="Chi siamo" />
+
+<section class="py-5">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-9">
+				<h1 class="fs-2 mb-0 mt-5">Nova Infissi</h1>
+				<h2 class="fs-4 mt-n1 mb-5 text-muted">standard di qualità e affidabilità</h2>
+				<p class="">
+					Ditta creata da Cenci Giuseppe Mirko nel 1999, quando, dopo molti anni di lavoro
+					dipendente con una azienda di falegnameria, Mirko decise di avviare un'attività propria,
+					specializzata nella vendita ed installazione di infissi. Già nei primi anni, dopo una
+					veloce fase di rodaggio, iniziò la vendita di infissi in legno.
+				</p>
+				<p>
+					Cenci Giuseppe Mirko ha sempre ritenuto questa realtà molto soddisfacente e creativa, ed
+					ora, dopo 20 di attività nel campo dei serramenti, è pronto ad offrire ai propri clienti
+					una gamma completa di prodotti: <strong
+						>porte interne, porte per garage, parapetti, tende da sole, scale interne infissi in PVC
+						e in legno alluminio</strong
+					>, e molto altro.
+				</p>
+				<p>
+					Nova Infissi ha due dipendenti addetti alla posa, sempre al passo con le ultime realtà del
+					mondo degli infissi attraverso corsi di aggiornamento periodici specifici.
+				</p>
+			</div>
+			<div class="col-md-3">
+				<picture>
+					<source srcset="/images/chi-siamo/chisiamo.jpg" type="image/jpg" />
+					<img
+						src="/images/chi-siamo/chisiamo.jpg"
+						class="w-100 object-fit-cover p-4 p-md-2"
+						alt="nova infissi chi siamo"
+					/>
+				</picture>
+			</div>
+		</div>
+	</div>
+</section>
