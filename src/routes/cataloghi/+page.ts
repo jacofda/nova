3087a7a6275ff9cc28a556426487dd89fde41834
Giacomo Gasperini <giacomo.gasperini@gmail.com>
2024-11-13 14:12:00 +0100

    add pagination to catalogues API request

--- src/routes/cataloghi/+page.ts
+++ src/routes/cataloghi/+page.ts
@@ -3,7 +3,8 @@ export async function load({ parent }) {
 	const { storyblokApi } = await parent();
 
 	const dataStory = await storyblokApi.get('cdn/stories', {
-		starts_with: 'catalogues/'
+		starts_with: 'catalogues/',
+		per_page: 100
 	});
 
 	const stories = dataStory.data.stories.map((story: any) => {
@@ -13,7 +14,6 @@ export async function load({ parent }) {
 			file: story.content.file
 		};
 	});
-
 	return {
 		story: stories
 	};
